8488a4957247d413dd2cf3dfb86d5e5a4bfc417a
SKT.조규형 <kyoohyung.cho@sk.com>
2020-03-15 10:26:10 +0900

    Chapter 3-3까지 완료

---
+++ src/main/java/com/kyoohyung/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,35 @@
+package com.kyoohyung.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ *  Entity 클래스에서는 절대로!! Setter를 만들지 않는다. 대신 해당 필드의 값 변경이 필요하면 명확히 그 목적과 의도를 나타낼수 있는 메소드를 추가해야 한다.
+ */
+@Getter // 클래스 내 모든 필드의 Getter 자동 생
+@NoArgsConstructor  // 기본 생성자 자동 생성
+@Entity //JPA 어노테이션 필수!!, 테이블과 링크퇼 클래스임을 알려준다.
+public class Posts {
+    @Id // 해당 테이블의 PK필드를 나타낸다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성규칙
+    private Long id;
+
+    @Column(length = 500, nullable = false) // @column은 추가 안해도 되는데 기본설정값 말고 다르게 사용하려면 지정하고 쓴다.
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder //빌더 패턴 클래스 생성, 생성자에 포함된 필드만 빌더에 포함됨.
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
---
+++ src/test/java/com/kyoohyung/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.kyoohyung.book.springboot.domain.posts;
+
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After  //JUnit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정, 보통 테스트간의 H2 가비지 데이터의 영향을 없애기 위해 처리함.
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        //postsRepository.save : insert/update 쿼리를 실행
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("author@gggmail.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();// 모든 데이터 조회
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
